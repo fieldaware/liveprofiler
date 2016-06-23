d7781f57c6e1f4ff223cc6b2ce59e02003c2b403
Piotr Szymanski <skyleton@gmail.com>
2016-06-23 13:32:25 +0200

    Add required info about package in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import io
 from setuptools import setup, find_packages
 read_file = lambda x: [l.strip() for l in open(x).readlines()]
 
 setup(
     name='liveprofiler',
-    version='0.0.1',
+    author='Piotr Szymanski',
+    author_email='piotr.szymanski@fieldaware.com',
+    url='https://github.com/fieldaware/liveprofiler',
+    license='MIT',
+    version='0.1.0',
+    description='Package for profiling WSGI applications on production',
+    long_description=io.open('README.md', encoding='utf-8').read(),
     packages=find_packages(),
-    install_requires=read_file("requirements.txt"),
+    install_requires=read_file('requirements.txt'),
     include_package_data=True,
     package_data={
         'static': 'liveprofiler/static/*',
