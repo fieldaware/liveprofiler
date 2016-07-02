9e05716f71c028fcb0b25953a450fb49f14e0648
Piotr Szymanski <skyleton@gmail.com>
2016-07-02 14:26:28 +0200

    use sampler for the visualizer itself by default

--- liveprofiler/app.py
+++ liveprofiler/app.py
@@ -4,13 +4,24 @@ import click
 
 from collector import collector
 from visualizer import visualizer
+import liveprofiler_sampler
+
+def wrap_with_profiling_middleware(app, config):
+    interval = config.get('interval')
+    secret_header = config.get('secret_header')
+    return liveprofiler_sampler.ProfilingMiddleware(app, interval, secret_header)
 
 def make_app(cfg_path):
     app = Flask('liveprofiler')
+
     cfg = get_config(cfg_path)
     app.config.update(**cfg)
+
     app.register_blueprint(collector)
     app.register_blueprint(visualizer)
+
+    app.wsgi_app = wrap_with_profiling_middleware(app.wsgi_app, cfg.get('sampler'))
+
     return app
 
 def get_config(path):
@@ -23,7 +34,6 @@ def get_config(path):
 
     cfg['collector']['hosts'] = cfg['collector']['hosts'].split(',')
     return cfg
-
 
 @click.command()
 @click.option('--cfg_path', type=str)
